7acc6441167c3a1e1310ee1ad3905868becce211
Elisa Castro de Souza <elisa.souza@fretebras.com.br>
2022-12-04 20:50:52 -0300

    feat: functions ok

--- src/service/validPassword.go
+++ src/service/validPassword.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
-	//"errors"
-	//"fmt"
+	"errors"
+	"fmt"
 	"sync"
+	"unicode"
+	//"strings"
 	//"time"
 
 	//"github.com/Eli15x/password/src/models"
@@ -38,29 +40,82 @@ func GetInstanceValidPassword() CommandValidPassword {
 
 func (vp *ValidPassword) MinSize(ctx context.Context, value int, password string) error {
 
+	if len(password) < value {
+		return errors.New("Error the password don't have the minSize")
+	}
 	return nil
 }
 
 func (vp *ValidPassword) MinUppercase(ctx context.Context, value int, password string) error {
 
+	upperCase := 0
+	for _, letter := range password {
+       if unicode.IsUpper(letter) {
+			upperCase++
+	   }   
+    }
+
+	if value > upperCase{
+		return errors.New("Error the password don't have the minUpperCase")
+	}
 	return nil
 }
 
 func (vp *ValidPassword) MinLowercase(ctx context.Context, value int, password string) error {
+	lowerCase := 0
+	for _, letter := range password {
+       if unicode.IsLower(letter) {
+			lowerCase++
+	   }   
+    }
+
+	if value > lowerCase{
+		return errors.New("Error the password don't have the minLowerCase")
+	}
+
 	return nil
 }
 
 func (vp *ValidPassword) MinDigit(ctx context.Context, value int, password string) error {
+	digit := 0
+	for _, letter := range password {
+       if unicode.IsDigit(letter) {
+			digit++
+	   }   
+    }
 
+	if value > digit{
+		return errors.New("Error the password don't have the min digits")
+	}
+
+	fmt.Println(digit)
 	return nil
 }
 
 func (vp *ValidPassword) MinSpecialChars(ctx context.Context, value int, password string) error  {
 
+	symbol := 0
+	for _, letter := range password {
+       if !unicode.IsDigit(letter) && !unicode.IsLetter(letter) {
+			symbol++
+	   }   
+    }
+
+	if value > symbol{
+		return errors.New("Error the password don't have the min special chars")
+	}
+
 	return nil
 }
 
 func (vp *ValidPassword) NoRepeted(ctx context.Context, value int, password string) error {
+	var letterFirst rune
+	for _, letter := range password {
+       if letterFirst == letter {
+			return errors.New("Error the password have Repeted letters")
+	   }  
+	   letterFirst = letter 
+    }
 
 	return nil
 }
